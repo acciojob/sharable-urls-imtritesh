ff61094fa9eebc45a9f7243148a50a8c7e652d3b
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-05-26 12:16:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // your code here
+let name = document.getElementById("name");
+let year = document.getElementById("year");
+let url = document.getElementById("url");
+let url1 = url.innerText;
+
+document.getElementById("button").addEventListener("click",function(){
+	event.preventDefault();
+	url.textContent = `${url1}?${name!=""?`name=${name.value}&`:""}${year!=""?`year=${year.value}`:""}`;
+});
